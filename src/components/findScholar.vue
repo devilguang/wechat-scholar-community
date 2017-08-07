869cb2bee9dfba1211ee48f9ae711e85e5f6e72a
devilguang <lzg@metadata.net.cn>
2017-08-07 09:59:15 +0800

    change code

--- src/components/findScholar.vue
+++ src/components/findScholar.vue
@@ -51,7 +51,7 @@
                 })
             },
             getQueryString(name){
-                var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+                var reg = new RegExp("(^|&)"    + name + "=([^&]*)(&|$)", "i");
                 var r = window.location.search.substr(1).match(reg);
                 if (r != null) return (r[2]);
                 return ''
